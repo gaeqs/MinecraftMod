57983d49dd8facfc745427001a8b9d0aa86ad276
baldo3 <baldorodriguezarbol@gmail.com>
2021-10-09 21:51:16 +0200

    add end torch, better end fences and obsidian bricks

--- src/main/kotlin/io/github/gaeqs/magicend/block/EndFenceStraightBlock.kt
+++ src/main/kotlin/io/github/gaeqs/magicend/block/EndFenceStraightBlock.kt
@@ -3,15 +3,12 @@ package io.github.gaeqs.magicend.block
 import io.github.gaeqs.magicend.MinecraftMod
 import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
-import net.minecraft.block.Block
-import net.minecraft.block.BlockState
-import net.minecraft.block.Material
-import net.minecraft.block.ShapeContext
+import net.minecraft.block.*
 import net.minecraft.item.BlockItem
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemPlacementContext
-import net.minecraft.particle.ParticleTypes
 import net.minecraft.state.StateManager
+import net.minecraft.state.property.DirectionProperty
 import net.minecraft.util.BlockRotation
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.BlockPos
@@ -19,34 +16,36 @@ import net.minecraft.util.math.Direction
 import net.minecraft.util.shape.VoxelShape
 import net.minecraft.util.shape.VoxelShapes
 import net.minecraft.world.BlockView
-import net.minecraft.world.World
-import java.util.*
 
-class DragonStatueBlock : Block(FabricBlockSettings.of(Material.STONE).strength(2.0f)) {
-
+class EndFenceStraightBlock : Block(FabricBlockSettings.of(Material.METAL).strength(2.0f)) {
 
     companion object {
-        val IDENTIFIER = Identifier(MinecraftMod.MOD_ID, "dragon_statue")
-        val BLOCK = DragonStatueBlock()
+        val FACING: DirectionProperty = HorizontalFacingBlock.FACING
+        val IDENTIFIER = Identifier(MinecraftMod.MOD_ID, "end_fence_straight")
+        val BLOCK = EndFenceStraightBlock()
         val BLOCK_ITEM = BlockItem(BLOCK, FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS))
+    }
+
+    init {
+        defaultState = stateManager.defaultState.with(FACING, Direction.NORTH)
     }
 
     override fun getOutlineShape(state: BlockState, world: BlockView, pos: BlockPos, context: ShapeContext)
             : VoxelShape {
-        return VoxelShapes.cuboid(0.1, 0.0, 0.1, 0.9, 0.8, 0.9)
+        return VoxelShapes.cuboid(0.25, 0.0, 0.25, 0.75, 1.0, 0.75)
     }
 
     override fun rotate(state: BlockState, rotation: BlockRotation): BlockState {
-        return state.with(EndWindowBlock.FACING, rotation.rotate(state.get(EndWindowBlock.FACING)) as Direction)
+        return state.with(FACING, rotation.rotate(state.get(FACING)) as Direction)
     }
 
     override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
-        builder.add(EndWindowBlock.FACING)
+        builder.add(FACING)
     }
 
     override fun getPlacementState(ctx: ItemPlacementContext): BlockState? {
         println(ctx.playerFacing.rotateYClockwise())
-        return super.getPlacementState(ctx)?.with(EndWindowBlock.FACING, ctx.playerFacing.rotateYClockwise())
+        return super.getPlacementState(ctx)?.with(FACING, ctx.playerFacing.rotateYClockwise())
     }
 
 
--- src/main/kotlin/io/github/gaeqs/magicend/block/EndTorch.kt
+++ src/main/kotlin/io/github/gaeqs/magicend/block/EndTorch.kt
@@ -9,44 +9,41 @@ import net.minecraft.block.Material
 import net.minecraft.block.ShapeContext
 import net.minecraft.item.BlockItem
 import net.minecraft.item.ItemGroup
-import net.minecraft.item.ItemPlacementContext
 import net.minecraft.particle.ParticleTypes
-import net.minecraft.state.StateManager
-import net.minecraft.util.BlockRotation
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.BlockPos
-import net.minecraft.util.math.Direction
 import net.minecraft.util.shape.VoxelShape
 import net.minecraft.util.shape.VoxelShapes
 import net.minecraft.world.BlockView
 import net.minecraft.world.World
 import java.util.*
 
-class DragonStatueBlock : Block(FabricBlockSettings.of(Material.STONE).strength(2.0f)) {
+class EndTorch : Block(FabricBlockSettings.of(Material.STONE).strength(2.0f)) {
 
 
     companion object {
-        val IDENTIFIER = Identifier(MinecraftMod.MOD_ID, "dragon_statue")
-        val BLOCK = DragonStatueBlock()
+        val IDENTIFIER = Identifier(MinecraftMod.MOD_ID, "end_torch")
+        val BLOCK = EndTorch()
         val BLOCK_ITEM = BlockItem(BLOCK, FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS))
     }
 
     override fun getOutlineShape(state: BlockState, world: BlockView, pos: BlockPos, context: ShapeContext)
             : VoxelShape {
-        return VoxelShapes.cuboid(0.1, 0.0, 0.1, 0.9, 0.8, 0.9)
+        return VoxelShapes.cuboid(0.35, 0.0, 0.35, 0.65, 1.0, 0.65)
     }
 
-    override fun rotate(state: BlockState, rotation: BlockRotation): BlockState {
-        return state.with(EndWindowBlock.FACING, rotation.rotate(state.get(EndWindowBlock.FACING)) as Direction)
-    }
 
-    override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
-        builder.add(EndWindowBlock.FACING)
-    }
+    override fun randomDisplayTick(state: BlockState, world: World, pos: BlockPos, random: Random) {
+        for (i in 0..1) {
+            var d = pos.x.toDouble() + 0.4 + (random.nextDouble()/5)
+            val e = pos.y.toDouble() + 0.4
+            var f = pos.z.toDouble() + 0.4 + (random.nextDouble()/5)
+            var g = 0.01
+            val h = 0.3
+            var j = 0.01
 
-    override fun getPlacementState(ctx: ItemPlacementContext): BlockState? {
-        println(ctx.playerFacing.rotateYClockwise())
-        return super.getPlacementState(ctx)?.with(EndWindowBlock.FACING, ctx.playerFacing.rotateYClockwise())
+            world.addParticle(ParticleTypes.PORTAL, d, e, f, g, h, j)
+        }
     }
 
 
