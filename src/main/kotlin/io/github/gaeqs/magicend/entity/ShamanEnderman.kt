f4898442b72894050d8c15970703bf36ec943084
Gael Rial Costas <gael.rial.costas@gmail.com>
2021-10-25 16:48:42 +0200

    Add shaman enderman.

--- src/main/kotlin/io/github/gaeqs/magicend/entity/ShamanEnderman.kt
+++ src/main/kotlin/io/github/gaeqs/magicend/entity/ShamanEnderman.kt
@@ -1,13 +1,17 @@
 package io.github.gaeqs.magicend.entity
 
+import com.google.common.collect.ImmutableList
+import com.mojang.datafixers.util.Pair
 import io.github.gaeqs.magicend.MinecraftMod
 import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricEntityTypeBuilder
 import net.minecraft.entity.EntityDimensions
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.SpawnGroup
-import net.minecraft.entity.ai.goal.EscapeDangerGoal
-import net.minecraft.entity.ai.goal.WanderAroundGoal
+import net.minecraft.entity.ai.brain.Activity
+import net.minecraft.entity.ai.brain.Brain
+import net.minecraft.entity.ai.brain.task.FollowMobTask
+import net.minecraft.entity.ai.brain.task.Task
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.data.DataTracker
@@ -17,8 +21,7 @@ import net.minecraft.util.Identifier
 import net.minecraft.util.math.Vec3d
 import net.minecraft.world.World
 
-class ShamanEnderman(type: EntityType<out PathAwareEntity>, world: World)
-    : PathAwareEntity(type, world), TopTextured {
+class ShamanEnderman(type: EntityType<out PathAwareEntity>, world: World) : PathAwareEntity(type, world), TopTextured {
 
     companion object {
         val IDENTIFIER = Identifier(MinecraftMod.MOD_ID, "shaman_enderman")
@@ -48,14 +51,14 @@ class ShamanEnderman(type: EntityType<out PathAwareEntity>, world: World)
 
     override var displayOffset = Vec3d(0.0, -2.5, 0.0)
 
+    init {
+        initBrain()
+    }
+
+
     override fun initDataTracker() {
         super.initDataTracker()
         dataTracker.startTracking(TRACKED_TOP_MESSAGE, MESSAGE_1.toString())
-    }
-
-    override fun initGoals() {
-        goalSelector.add(0, EscapeDangerGoal(this, 2.0))
-        goalSelector.add(1, WanderAroundGoal(this, 1.0, 100))
     }
 
     override fun setAttacker(attacker: LivingEntity?) {
@@ -68,5 +71,24 @@ class ShamanEnderman(type: EntityType<out PathAwareEntity>, world: World)
         println(brain)
     }
 
+
+    override fun getBrain(): Brain<ShamanEnderman> {
+        return super.getBrain() as Brain<ShamanEnderman>
+    }
+
+    private fun initBrain() {
+        val brain = getBrain();
+
+        brain.setTaskList(Activity.IDLE, ImmutableList.of(createFreeFollowTask()))
+        brain.setDefaultActivity(Activity.IDLE)
+        brain.doExclusively(Activity.IDLE)
+    }
+
+    private fun createFreeFollowTask(): Pair<Int, Task<LivingEntity?>>? {
+        return Pair.of(
+            5,
+            FollowMobTask(EntityType.CAT, 8.0f)
+        )
+    }
 
 }
