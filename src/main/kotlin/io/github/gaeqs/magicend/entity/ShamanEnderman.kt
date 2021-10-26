e3330e5480ac3e3f03510406e17bf4130cf8ec08
Gael Rial Costas <gael.rial.costas@gmail.com>
2021-10-26 14:05:55 +0200

    Walking shaman.

--- src/main/kotlin/io/github/gaeqs/magicend/entity/ShamanEnderman.kt
+++ src/main/kotlin/io/github/gaeqs/magicend/entity/ShamanEnderman.kt
@@ -1,7 +1,8 @@
 package io.github.gaeqs.magicend.entity
 
 import com.google.common.collect.ImmutableList
-import com.mojang.datafixers.util.Pair
+import com.google.common.collect.ImmutableSet
+import com.mojang.serialization.Dynamic
 import io.github.gaeqs.magicend.MinecraftMod
 import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricEntityTypeBuilder
 import net.minecraft.entity.EntityDimensions
@@ -10,13 +11,15 @@ import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.SpawnGroup
 import net.minecraft.entity.ai.brain.Activity
 import net.minecraft.entity.ai.brain.Brain
-import net.minecraft.entity.ai.brain.task.FollowMobTask
-import net.minecraft.entity.ai.brain.task.Task
+import net.minecraft.entity.ai.brain.MemoryModuleType
+import net.minecraft.entity.ai.brain.sensor.SensorType
+import net.minecraft.entity.ai.brain.task.*
 import net.minecraft.entity.attribute.DefaultAttributeContainer
 import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.entity.data.DataTracker
 import net.minecraft.entity.data.TrackedDataHandlerRegistry
 import net.minecraft.entity.mob.PathAwareEntity
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.Vec3d
 import net.minecraft.world.World
@@ -29,6 +32,16 @@ class ShamanEnderman(type: EntityType<out PathAwareEntity>, world: World) : Path
             SpawnGroup.CREATURE,
             EntityType.EntityFactory<ShamanEnderman> { type, world -> ShamanEnderman(type, world) }
         ).dimensions(EntityDimensions.fixed(0.8f, 3.0f)).build()
+
+        private val SENSOR_TYPES = listOf(SensorType.NEAREST_LIVING_ENTITIES)
+        private val MEMORY_MODULE_TYPES = listOf(
+            MemoryModuleType.MOBS,
+            MemoryModuleType.VISIBLE_MOBS,
+            MemoryModuleType.LOOK_TARGET,
+            MemoryModuleType.CANT_REACH_WALK_TARGET_SINCE,
+            MemoryModuleType.PATH,
+            MemoryModuleType.WALK_TARGET
+        )
 
         private val TRACKED_TOP_MESSAGE =
             DataTracker.registerData(ShamanEnderman::class.java, TrackedDataHandlerRegistry.STRING)
@@ -51,11 +64,6 @@ class ShamanEnderman(type: EntityType<out PathAwareEntity>, world: World) : Path
 
     override var displayOffset = Vec3d(0.0, -2.5, 0.0)
 
-    init {
-        initBrain()
-    }
-
-
     override fun initDataTracker() {
         super.initDataTracker()
         dataTracker.startTracking(TRACKED_TOP_MESSAGE, MESSAGE_1.toString())
@@ -66,29 +74,36 @@ class ShamanEnderman(type: EntityType<out PathAwareEntity>, world: World) : Path
         topTexture = if (attacker == null) MESSAGE_1 else MESSAGE_2
     }
 
-    override fun tick() {
-        super.tick()
-        println(brain)
-    }
-
-
     override fun getBrain(): Brain<ShamanEnderman> {
         return super.getBrain() as Brain<ShamanEnderman>
     }
 
-    private fun initBrain() {
-        val brain = getBrain();
-
-        brain.setTaskList(Activity.IDLE, ImmutableList.of(createFreeFollowTask()))
-        brain.setDefaultActivity(Activity.IDLE)
-        brain.doExclusively(Activity.IDLE)
+    override fun createBrainProfile(): Brain.Profile<*> {
+        return Brain.createProfile(MEMORY_MODULE_TYPES, SENSOR_TYPES)
     }
 
-    private fun createFreeFollowTask(): Pair<Int, Task<LivingEntity?>>? {
-        return Pair.of(
-            5,
-            FollowMobTask(EntityType.CAT, 8.0f)
-        )
+    override fun deserializeBrain(dynamic: Dynamic<*>?): Brain<*> {
+        val brain = super.deserializeBrain(dynamic) as Brain<ShamanEnderman>
+        initBrain(brain)
+        return brain
+    }
+
+    override fun mobTick() {
+        world.profiler.push("shamanBrain")
+        getBrain().tick(world as ServerWorld, this)
+        world.profiler.pop()
+    }
+
+    private fun initBrain(brain: Brain<ShamanEnderman>) {
+        brain.setTaskList(Activity.IDLE, 0, ImmutableList.of(
+            FollowMobTask(EntityType.PLAYER, 8.0f),
+            WanderAroundTask()
+        ))
+        brain.setTaskList(Activity.CORE, 0, ImmutableList.of(LookAroundTask(45, 90), StrollTask(1.0f)))
+
+        brain.setCoreActivities(ImmutableSet.of(Activity.CORE))
+        brain.setDefaultActivity(Activity.IDLE)
+        brain.resetPossibleActivities()
     }
 
 }
